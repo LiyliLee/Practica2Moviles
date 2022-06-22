4adc8dc9bee05bb79fd7c54ed1c201c4cf47b18c
GCidon <gcidonch@ucm.es>
2022-06-22 11:17:15 +0200

    cosa

--- FlowFree/Assets/script/UIScripts/FinishPanel.cs
+++ FlowFree/Assets/script/UIScripts/FinishPanel.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 using TMPro;
 
 public class FinishPanel : MonoBehaviour
 {
 
+    string gameId = "4508761";
+
     public TMP_Text perfectText_;
     public TMP_Text stepsText_;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId);
+    }
+
+    public void ShowAd()
+    {
+        Advertisement.Show();
+    }
 
     public void SetFinishPanel(bool perfect, int steps)
     {
@@ -15,5 +28,7 @@ public class FinishPanel : MonoBehaviour
         else perfectText_.text = "¡Nivel completado!";
 
         stepsText_.text = "Completaste el nivel en " + steps + " pasos.";
+
+        ShowAd();
     }
 }
